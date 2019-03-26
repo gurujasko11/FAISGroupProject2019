fe7c7f8c2c3af5820fc13e4cf86704dd34cb1db3
piotrintes <48256391+piotrintes@users.noreply.github.com>
2019-03-26 09:28:58 +0100

    Some minor fixes + routing example
    Drobna poprawka w css - tabele muszą mieć class=table aby być formatowane, w przeciwnym wypadku formatowanie będzie domyślne; zmiana ta będzie niezbędna do tworzenia formularzy.
    
    Dodałem przykładowy routing i lekko rozbudowałem szablon headera (główne menu), żeby pokazać co miałem na myśli; dodając stronę należy dodać znacznik w header:
    
    <a class=
                    <% if(title=="[tytuł nowej strony]"){ %>
                        menu_s
                    <% } else{ %>
                        menu
                    <% } %> href="[adres nowej strony]" tabindex="[numer linku]">[nazwa wyświetlana na przycisku]</a>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { page: 'index', title: 'Express' });
+});
+
+router.get('/exp1', function(req, res, next) {
+  res.render('index', { page: 'index', title: 'Express1' });
 });
 
 module.exports = router;
