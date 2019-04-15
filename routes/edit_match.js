5fee80e8cb35fbcc77b73d3461d0bf323649a7e8
kamilo116 <kamil.sladowski@gmail.com>
2019-04-15 23:31:28 +0200

    materialize in edit match (todo add time picker)

--- routes/edit_match.js
+++ routes/edit_match.js
@@ -4,22 +4,11 @@ var router = express.Router();
 
 router.get('/', function(req, res, next)
 {
-    res.send('Got a GET request')
-    // res.render('add_match');
+    res.render('edit_match', { page: 'main', title: 'Edycja rozgrywki' });
 });
 
 router.post('/', function(req, res, next)
 {
-    res.send(req.toString());
-
-    if (req.action === 'Edytuj'){
-        res.send('update')
-    } else if (req.action === 'delete_button') {
-        res.send('delete')
-    } else {
-        res.send('no button preseed')
-    }
-
 
     // res.send('Got a POST request')
     res.render('edit_match', { page: 'main', title: 'Edycja rozgrywki' });
