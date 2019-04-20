3d10ec87821eab529beaedb19ee982b0d66cd871
Rafal Lebioda <rafallebioda@Rafals-MacBook-Air.local>
2019-04-20 17:20:18 +0200

    Added alerts for login, fixed bugs

--- routes/index.js
+++ routes/index.js
@@ -10,23 +10,26 @@ router.use(passport.session());
 
 passport.use(new LocalStrategy({
   usernameField: 'email',
-  passwordField: 'password'
+  passwordField: 'password',
+  passReqToCallback : true
 },
-function(username, password, done) {
-  console.log(username);
-  console.log(password);
-  if(!username || !password ) { console.log("username/password not given: "); console.log(username); console.log(password); return done(null, false); }
-  dbconn.query("select * from Uzytkownicy where email = '"+ username+"'", function(err, rows){
+  function(req, username, password, done) {
+    username = username.replace("'", "''");
+    password = password.replace("'", "''");
+    console.log(username);
+    console.log(password);
+    if(!username || !password ) { console.log("username/password not given: "); console.log(username); console.log(password); return done(null, false); }
+      dbconn.query("select * from Uzytkownicy where email = '"+ username+"'", function(err, rows){
       console.log(err); console.log(rows);
-      if (err) return done(req.flash('message',err));
-      if(!rows.length){ return done(null, false, { type: 'ERROR', msg: 'Błąd' }); }
+      if (err) return done(null, false, req.flash('FLASH_MSG', ['SQL ERROR', err]));
+      if(!rows.length){ return done(null, false, req.flash('FLASH_MSG', ['ERROR', 'Użytkownik z takim e-mailem nie istnieje'])); }
 
       var encPassword = password;
       var dbPassword  = rows[0].haslo;
       if(!(dbPassword == encPassword)){
-          return done(null, false, { type: 'ERROR', msg: 'Błąd' });
-        }
-      return done(null, rows[0], { type: 'SUCCESS', msg: 'Zalogowano pomyślnie' });
+        return done(null, false, req.flash('FLASH_MSG', ['ERROR', 'Niepoprawne hasło']));
+      }
+      return done(null, rows[0]);
     });
   }
 ));
@@ -67,14 +70,14 @@ router.get('/register_bar', function(req, res, next)
 router.post('/register_bar', function(req, res, next)
 {
   //TODO: escape '
-  var bar_name = req.body.bar_name;
-  var telephone = req.body.telephone;
-  var city = req.body.city;
-  var street = req.body.street;
-  var building_number = req.body.building_number;
-  var local_number = req.body.local_number;
-  var password = req.body.password;
-  var email = req.body.email;
+  var bar_name = req.body.bar_name.replace("'", "''");
+  var telephone = req.body.telephone.replace("'", "''");
+  var city = req.body.city.replace("'", "''");
+  var street = req.body.street.replace("'", "''");
+  var building_number = req.body.building_number.replace("'", "''");
+  var local_number = req.body.local_number.replace("'", "''");
+  var password = req.body.password.replace("'", "''");
+  var email = req.body.email.replace("'", "''");
   var query = "insert into Bary (nazwa_baru, telefon, miasto, ulica, numer_budynku, numer_lokalu, haslo, email) values " +
               "('" + bar_name + "', '" + telephone + "', '" + city + "', '" + street + "', '" + building_number + "', '" + local_number + "', '" + password + "', '" + email + "');";
 
@@ -93,12 +96,11 @@ router.get('/register_user', function(req, res, next)
 
 router.post('/register_user', function(req, res, next)
 {
-  //TODO: escape '
-  var first_name = req.body.first_name;
-  var last_name = req.body.last_name;
-  var telephone = req.body.telephone;
-  var password = req.body.password;
-  var email = req.body.email;
+  var first_name = req.body.first_name.replace("'", "''");
+  var last_name = req.body.last_name.replace("'", "''");
+  var telephone = req.body.telephone.replace("'", "''");
+  var password = req.body.password.replace("'", "''");
+  var email = req.body.email.replace("'", "''");
   if(telephone == '') telephone = 'NULL';
   var query = "insert into Uzytkownicy (imie, nazwisko, email, telefon, haslo) values " +
               "('" + first_name + "', '" + last_name + "', '" + email + "', '" + telephone + "', '" + password + "');";
@@ -113,7 +115,7 @@ router.post('/register_user', function(req, res, next)
 
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { page: 'main', title: 'Logowanie' });
+  res.render('login', { page: 'main', title: 'Logowanie', flash_messages: req.flash('FLASH_MSG') });
 });
 
 router.post('/login', 
