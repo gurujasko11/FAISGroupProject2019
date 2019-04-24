0ddce50eef4de61d379b9a867ff6b41eaf879b6a
michal_bator <michalbator199@gmail.com>
2019-04-24 21:48:51 +0200

    rozgrywki

--- routes/index.js
+++ routes/index.js
@@ -124,6 +124,7 @@ router.post('/register_bar', function(req, res, next)
       console.log("Wyslano insert do bazy danych: " + query);
       dbconn.query(query, function(err, rows)
       {
+          console.log(err);
         if(err) res.render('register_bar', { page: getPageVariable(req), title: "Rejestracja baru", type: 'ERROR', msg: "Ten e-mail już jest zajęty" });
         else res.render('register_bar', { page: getPageVariable(req), title: "Rejestracja baru", type: 'SUCCESS', msg: "Pomyślnie utworzono konto." });
       });
@@ -209,5 +210,127 @@ router.get('/test', function(req, res)
   if(req.isAuthenticated()) console.log("[/TEST] req.user = " + JSON.stringify(req.user, null, 3));
   res.redirect('/');
 });
+
+router.get('/edit_bar', function (req, res, next) {
+    if (req.isAuthenticated()){
+        var query = "SELECT nazwa_baru, telefon, miasto, ulica, numer_budynku, numer_lokalu, haslo, email " +
+            "FROM Bary " +
+            "WHERE id_baru = '" + req.user.barID + "'";
+
+        dbconn.query(query, function (err, rows) {
+            // console.log(err);
+            console.log(rows);
+
+            if (err) res.render('edit_bar', {
+                page: getPageVariable(req),
+                title: "Edycja baru",
+                type: 'ERROR',
+                msg: "Cos poszło nie tak z pobraniem danych konta",
+                bar: null
+            });
+            else res.render('edit_bar', {
+                page: getPageVariable(req),
+                title: "Edycja baru",
+                type: 'SUCCESS',
+                msg: "Pomyślnie pobrano dane konta.",
+                bar: rows[0]
+            });
+        });
+    }
+    else{
+        res.redirect('/');
+    }
+
+
+
+});
+
+function get_bar_data(barID) {
+    return new Promise(function (resolve, reject) {
+
+        var query = "SELECT nazwa_baru, telefon, miasto, ulica, numer_budynku, numer_lokalu, haslo, email " +
+            "FROM Bary " +
+            "WHERE id_baru = '" + barID + "'";
+        dbconn.query(query, function (err, rows) {
+            // console.log(err);
+            console.log(rows);
+            if (!err)
+                resolve(rows[0]);
+            else
+                reject(err);
+        });
+
+
+    })
+}
+
+router.post('/edit_bar', function (req, res, next) {
+    var bar_name = "'" + req.body.bar_name.replace("'", "''") + "'";
+    var telephone = "'" + req.body.telephone.replace("'", "''") + "'";
+    var city = "'" + req.body.city.replace("'", "''") + "'";
+    var street = "'" + req.body.street.replace("'", "''") + "'";
+    var building_number = "'" + req.body.building_number.replace("'", "''") + "'";
+    var local_number = "'" + req.body.local_number.replace("'", "''") + "'";
+    var password = "'" + req.body.password.replace("'", "''") + "'";
+    var email = "'" + req.body.email.replace("'", "''") + "'";
+
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+
+            var query = "UPDATE Bary " +
+                "SET nazwa_baru = " + bar_name + ", telefon = " + telephone + ", miasto = " + city + ", ulica = " + street
+                + ", numer_budynku = " + building_number + ", numer_lokalu = " + local_number + ", haslo = '" + hash +
+                "', email = " + email +
+                " WHERE id_baru = '" + req.user.barID + "'";
+
+
+            console.log("Wyslano update do bazy danych: " + query);
+            dbconn.query(query, function (err, rows) {
+                console.log(err);
+
+                get_bar_data(req.user.barID)
+                    .then(function (row) {
+
+                        if (err) res.render('edit_bar', {
+                            page: getPageVariable(req),
+                            title: "Edycja baru",
+                            type: 'ERROR',
+                            msg: "Ten e-mail już jest zajęty",
+                            bar: null
+                        });
+                        else res.render('edit_bar', {
+                            page: getPageVariable(req),
+                            title: "Edycja baru",
+                            type: 'SUCCESS',
+                            msg: "Pomyślnie zmieniono konto.",
+                            bar: row
+                        });
+                    })
+
+            });
+        });
+    });
+});
+
+router.get('/delete_bar', function (req, res, next) {
+    if (! req.isAuthenticated()){
+        res.redirect('/');
+    }
+    res.render('delete_bar', {page: 'main', title: 'Usuń bar'});
+});
+
+
+router.post('/delete_bar', function (req, res, next) {
+    var query = "DELETE FROM Bary WHERE id_baru = '" + req.user.barID + "'";
+    dbconn.query(query, function (err, rows) {
+        console.log(err);
+        req.logout();
+        res.redirect('/');
+
+    })
+
+});
+
 
 module.exports = router;
