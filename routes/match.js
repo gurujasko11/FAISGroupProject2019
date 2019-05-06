803228f0d19e290f806b2338653674e7e1aab408
kamilo116 <kamil.sladowski@gmail.com>
2019-05-06 21:15:50 +0200

    added delete match functionality

--- routes/match.js
+++ routes/match.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// SELECT Mecze.id_meczu as id, Mecze.czas, dr1.nazwa_druzyny as team1, dr2.nazwa_druzyny as team2 FROM Mecze LEFT JOIN Druzyny dr1 ON dr1.id_druzyny = Mecze.id_druzyna1 LEFT JOIN Druzyny dr2 ON dr2.id_druzyny = Mecze.id_druzyna2;
+
 router.get('/', function (req, res, next) {
-        dbconn.query("SELECT Mecze.id_meczu as id, Mecze.czas, dr1.nazwa_druzyny as team1, dr2.nazwa_druzyny as team2 FROM Mecze LEFT JOIN Druzyny dr1 ON dr1.id_druzyny = Mecze.id_druzyna1 LEFT JOIN Druzyny dr2 ON dr2.id_druzyny = Mecze.id_druzyna2;", function (err, result) {
+        dbconn.query("SELECT Mecze.id_meczu as id, Mecze.czas, dr1.nazwa_druzyny as team1, dr2.nazwa_druzyny as team2 " +
+            "FROM Mecze LEFT JOIN Druzyny dr1 ON dr1.id_druzyny = Mecze.id_druzyna1 " +
+            "LEFT JOIN Druzyny dr2 ON dr2.id_druzyny = Mecze.id_druzyna2;", function (err, result) {
         let emptyArray = [];
 
         if(result === undefined) {
@@ -21,6 +25,7 @@ router.post('/add', function (req, res, next) {
     let team1_id, team2_id;
     dbconn.query("INSERT INTO Druzyny(nazwa_druzyny) VALUES ('" + req.body.team1 + "')", function (err, result) {
         team1_id = result.insertId;
+
         dbconn.query("INSERT INTO Druzyny(nazwa_druzyny) VALUES ('" + req.body.team2 + "')", function (err, result) {
             team2_id = result.insertId;
             let datetime = req.body.date + " " + req.body.time;
@@ -33,9 +38,14 @@ router.post('/add', function (req, res, next) {
 });
 
 
+//SELECT Mecze.id_meczu as id, Mecze.czas, dr1.nazwa_druzyny as team1, dr2.nazwa_druzyny as team2 FROM Mecze LEFT JOIN Druzyny dr1 ON dr1.id_druzyny = Mecze.id_druzyna1 LEFT JOIN Druzyny dr2 ON dr2.id_druzyny = Mecze.id_druzyna2 WHERE id_meczu = 1;
+
 router.get('/edit/:id', function (req, res, next) {
     console.log(req.params.id);
-    dbconn.query("SELECT Mecze.id_meczu as id, Mecze.czas, dr1.nazwa_druzyny as team1, dr2.nazwa_druzyny as team2 FROM Mecze LEFT JOIN Druzyny dr1 ON dr1.id_druzyny = Mecze.id_druzyna1 LEFT JOIN Druzyny dr2 ON dr2.id_druzyny = Mecze.id_druzyna2 WHERE id_meczu =" + req.params.id, function (err, result) {
+    dbconn.query("SELECT Mecze.id_meczu as id, Mecze.czas, dr1.nazwa_druzyny as team1, dr2.nazwa_druzyny as team2 " +
+        "FROM Mecze LEFT JOIN Druzyny dr1 ON dr1.id_druzyny = Mecze.id_druzyna1 " +
+        "LEFT JOIN Druzyny dr2 ON dr2.id_druzyny = Mecze.id_druzyna2 WHERE id_meczu ="
+        + req.params.id, function (err, result) {
         result[0].czas = result[0].czas + "";
         console.log(result[0].czas);
         res.render('edit_match', {page: 'main', title: 'Edycja rozgrywki', data: result[0]});
@@ -46,5 +56,32 @@ router.post('/edit', function (req, res, next) {
 
     res.render('edit_match', {page: 'main', title: 'Edycja rozgrywki'});
 });
+
+
+router.get('/delete/:id', function (req, res, next) {
+    console.log(req.params.id);
+    dbconn.query("select * from Mecze where id_meczu="
+        + req.params.id, function (err, result) {
+
+        dbconn.query("SET FOREIGN_KEY_CHECKS=0;", function (err, res) {
+        });
+
+        dbconn.query("delete from Druzyny where id_druzyny="
+            + result[0].id_druzyna1, function (err, res) {
+            console.log(result[0].id_druzyna1);
+        });
+        dbconn.query("delete from Druzyny where id_druzyny="
+            + result[0].id_druzyna2, function (err, res) {
+            console.log(result[0].id_druzyna2);
+        });
+        dbconn.query("delete from Mecze where id_meczu="
+            + req.params.id, function (err, res) {
+        });
+        dbconn.query("SET FOREIGN_KEY_CHECKS=1;", function (err, res) {
+        });
+        res.redirect('/match');
+    });
+});
+
 
 module.exports = router;
