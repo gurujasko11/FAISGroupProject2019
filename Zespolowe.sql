a5adfcc30772d2b764888060a9e2b47bc2f883cd
mateuszdobija <mat789789@gmail.com>
2019-06-06 08:31:44 +0200

    Baza

--- Zespolowe.sql
+++ Zespolowe.sql
@@ -34,11 +34,12 @@ CREATE TABLE `Bary` (
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `longitude` float(10,6) DEFAULT NULL,
   `latitude` float(10,6) DEFAULT NULL,
+  `status` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_baru`),
   UNIQUE KEY `email` (`email`),
   KEY `miasto` (`miasto`),
   CONSTRAINT `Bary_ibfk_1` FOREIGN KEY (`miasto`) REFERENCES `Miasta` (`miasto`)
-) ENGINE=InnoDB AUTO_INCREMENT=1116 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1120 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +48,7 @@ CREATE TABLE `Bary` (
 
 LOCK TABLES `Bary` WRITE;
 /*!40000 ALTER TABLE `Bary` DISABLE KEYS */;
-INSERT INTO `Bary` VALUES (1112,'Pueblo1','111111111','Kraków','Lubicz','2','','$2b$10$wngZg3DNIxDNST8.KFyCxe7GX5YmQPTuzcYIJuN3UYs4LrEw37G0.','pueblo1@pueblo.com',NULL,NULL),(1113,'SuperBar','123456789','Krakow','Krakowska','12','12','$2b$10$P9Rgh50UEfZ1UOM.QD0d3OM4ZCLdMLp8gKBscPNsybuu4EiVBo1xW','super@bar.pl',NULL,NULL),(1114,'bar','111111111','Kraków','Łojasiewicza','1','','$2b$10$uex8UlHbQUOVXqjx2MQmeeN4Oap63RVvRAPaRgNCIwFgDJg3bG0c2','bar@bar.com',NULL,NULL),(1115,'barek','111111111','Kraków','Łojasiewicza','1','','$2b$10$zkYaWiBMFCpM69OnwAePSOyR/NP5xCf33n7ncYSXZmWpe5mftjI3.','barek@barek.com',NULL,NULL);
+INSERT INTO `Bary` VALUES (1112,'Pueblo1','111111111','Kraków','Lubicz','2','','$2b$10$wngZg3DNIxDNST8.KFyCxe7GX5YmQPTuzcYIJuN3UYs4LrEw37G0.','pueblo1@pueblo.com',NULL,NULL,NULL),(1113,'SuperBar','123456789','Krakow','Krakowska','12','12','$2b$10$P9Rgh50UEfZ1UOM.QD0d3OM4ZCLdMLp8gKBscPNsybuu4EiVBo1xW','super@bar.pl',NULL,NULL,NULL),(1114,'bar','111111111','Kraków','Łojasiewicza','1','','$2b$10$uex8UlHbQUOVXqjx2MQmeeN4Oap63RVvRAPaRgNCIwFgDJg3bG0c2','bar@bar.com',NULL,NULL,NULL),(1118,'barek','11111111','bb','b','1','','$2b$10$mEPe.RlYRFYMNpRlHAVigOV9Gs59msiMrLxrXsfIwbqckAF0l7E/.','barek@barek.com',NULL,NULL,NULL),(1119,'aaaaaa','11111111','mmmm','a','1','','$2b$10$4f6XNgmPDQ2AZ8PuQHG33.nd3FZ505qvTqcYqJWqPFVpAyuhjH6FK','b@b.com',NULL,NULL,NULL);
 /*!40000 ALTER TABLE `Bary` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -68,7 +69,7 @@ CREATE TABLE `Bary_Z_Meczami` (
   KEY `id_meczu` (`id_meczu`),
   CONSTRAINT `Bary_Z_Meczami_ibfk_1` FOREIGN KEY (`id_baru`) REFERENCES `Bary` (`id_baru`),
   CONSTRAINT `Bary_Z_Meczami_ibfk_2` FOREIGN KEY (`id_meczu`) REFERENCES `Mecze` (`id_meczu`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -77,6 +78,7 @@ CREATE TABLE `Bary_Z_Meczami` (
 
 LOCK TABLES `Bary_Z_Meczami` WRITE;
 /*!40000 ALTER TABLE `Bary_Z_Meczami` DISABLE KEYS */;
+INSERT INTO `Bary_Z_Meczami` VALUES (1,1113,2,'2019-06-13 11:11:00'),(2,1113,3,'2019-06-06 11:11:00'),(3,1113,4,'2019-06-20 11:11:00');
 /*!40000 ALTER TABLE `Bary_Z_Meczami` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -92,7 +94,7 @@ CREATE TABLE `Druzyny` (
   `nazwa_druzyny` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_druzyny`),
   UNIQUE KEY `nazwa_druzyny` (`nazwa_druzyny`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,7 +103,6 @@ CREATE TABLE `Druzyny` (
 
 LOCK TABLES `Druzyny` WRITE;
 /*!40000 ALTER TABLE `Druzyny` DISABLE KEYS */;
-INSERT INTO `Druzyny` VALUES (1,'legia'),(2,'pogoń');
 /*!40000 ALTER TABLE `Druzyny` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -122,7 +123,7 @@ CREATE TABLE `Mecze` (
   KEY `id_druzyna2` (`id_druzyna2`),
   CONSTRAINT `Mecze_ibfk_1` FOREIGN KEY (`id_druzyna1`) REFERENCES `Druzyny` (`id_druzyny`),
   CONSTRAINT `Mecze_ibfk_2` FOREIGN KEY (`id_druzyna2`) REFERENCES `Druzyny` (`id_druzyny`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,7 +132,6 @@ CREATE TABLE `Mecze` (
 
 LOCK TABLES `Mecze` WRITE;
 /*!40000 ALTER TABLE `Mecze` DISABLE KEYS */;
-INSERT INTO `Mecze` VALUES (1,1,2,'2019-05-08 00:00:00');
 /*!40000 ALTER TABLE `Mecze` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +154,7 @@ CREATE TABLE `Miasta` (
 
 LOCK TABLES `Miasta` WRITE;
 /*!40000 ALTER TABLE `Miasta` DISABLE KEYS */;
-INSERT INTO `Miasta` VALUES ('Gdynia'),('Katowice'),('Kraków'),('Poznań'),('Warszawa'),('Wrocław');
+INSERT INTO `Miasta` VALUES ('BB'),('Gdynia'),('Katowice'),('Krak'),('Kraków'),('mmmm'),('Poznań'),('Warszawa'),('Wrocław');
 /*!40000 ALTER TABLE `Miasta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -175,7 +175,7 @@ CREATE TABLE `Uzytkownicy` (
   `status` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id_uzytkownika`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1239 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -184,8 +184,7 @@ CREATE TABLE `Uzytkownicy` (
 
 LOCK TABLES `Uzytkownicy` WRITE;
 /*!40000 ALTER TABLE `Uzytkownicy` DISABLE KEYS */;
-INSERT INTO `Uzytkownicy` VALUES (1234, "Admin", "Admin", "admin@admin.com", 123456789, 1234, NULL);
-/*INSERT INTO `Uzytkownicy` VALUES (1235, "Admin", "Admin", "chce.dostac@maila.com", 123456789, 1234, NULL);*/;
+INSERT INTO `Uzytkownicy` VALUES (1234,'Admin','Admin','admin@admin.com','123456789','1234',NULL),(1235,'aaaa','bbbb','barek@barek.com','NULL','$2b$10$P25gC8O0vCRODQYw3E5YOuhx/10iodvUC7O9sJUiywfzZSYvCQGCm',NULL),(1238,'NULL','NULL','mat789789@gmail.com','NULL','$2b$10$VnMz5DymURdOLm1tAHHP4uQyapYoVLX2z1DseBvm0BUkLf1h2bUYO','activated');
 /*!40000 ALTER TABLE `Uzytkownicy` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -212,13 +211,11 @@ CREATE TABLE `Uzytkownik_Z_Druzynami` (
 
 LOCK TABLES `Uzytkownik_Z_Druzynami` WRITE;
 /*!40000 ALTER TABLE `Uzytkownik_Z_Druzynami` DISABLE KEYS */;
-INSERT INTO `Uzytkownik_Z_Druzynami` VALUES (1235, 1);
 /*!40000 ALTER TABLE `Uzytkownik_Z_Druzynami` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
 --
--- Table structure for table `Uzytkownik_Z_Druzynami`
+-- Table structure for table `Wlasciciel_Z_Barami`
 --
 
 DROP TABLE IF EXISTS `Wlasciciel_Z_Barami`;
@@ -228,8 +225,6 @@ CREATE TABLE `Wlasciciel_Z_Barami` (
   `id_uzytkownika` int(11) NOT NULL,
   `id_baru` int(11) NOT NULL,
   PRIMARY KEY (`id_uzytkownika`)
---  CONSTRAINT `Wlasciciel_Z_Barami_ibfk_1` FOREIGN KEY (`id_uzytkownika`) REFERENCES `Uzytkownicy` (`id_uzytkownika`),
---  CONSTRAINT `Wlasciciel_Z_Barami_ibfk_2` FOREIGN KEY (`id_druzyny`) REFERENCES `Druzyny` (`id_druzyny`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -239,12 +234,9 @@ CREATE TABLE `Wlasciciel_Z_Barami` (
 
 LOCK TABLES `Wlasciciel_Z_Barami` WRITE;
 /*!40000 ALTER TABLE `Wlasciciel_Z_Barami` DISABLE KEYS */;
-INSERT INTO `Wlasciciel_Z_Barami` VALUES (1234, 1112);
-INSERT INTO `Wlasciciel_Z_Barami` VALUES (1235, 1113);
+INSERT INTO `Wlasciciel_Z_Barami` VALUES (1234,1112),(1235,1113);
 /*!40000 ALTER TABLE `Wlasciciel_Z_Barami` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -255,4 +247,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-07 22:28:02
+-- Dump completed on 2019-06-06  8:29:26
